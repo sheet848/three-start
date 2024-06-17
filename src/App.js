8ebb4050d86773e55279dcb8fad6d6c3a34481ac
sheet848 <160768361+sheet848@users.noreply.github.com>
2024-06-17 13:42:57 +0530

    first commit for react and three js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MyThree from './three';
+
+function App() {
+  return (
+    <div className="App">
+      <MyThree />
+    </div>
+  );
+}
+
+export default App;
